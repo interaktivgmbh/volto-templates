37751072ccda9a574f53430932a9474d40f985a5
mxurice-rm <reim@interaktiv.de>
2025-04-29 09:33:33 +0200

    added actions for thumbnail generation

--- src/actions/templates/index.js
+++ src/actions/templates/index.js
@@ -1,22 +1,38 @@
 import {
-  GET_SELECTABLE_TEMPLATES,
-  TOGGLE_SHOW_TEMPLATES_MODAL
+    GET_SELECTABLE_TEMPLATES,
+    SET_TEMPLATE_THUMBNAIL_CALLBACK,
+    TOGGLE_SHOW_TEMPLATES_MODAL,
+    TRIGGER_TEMPLATE_THUMBNAIL
 } from "../../constants/ActionTypes";
 
 export function getSelectableTemplates() {
-  const path = '/@search';
+    const path = '/@search';
 
-  return {
-    type: GET_SELECTABLE_TEMPLATES,
-    request: {
-      op: 'get',
-      path: `/@search?portal_type=Template&metadata_fields=UID&metadata_fields=template_description&metadata_fields=template_thumbnail`,
-    },
-  };
+    return {
+        type: GET_SELECTABLE_TEMPLATES,
+        request: {
+            op: 'get',
+            path: `/@search?portal_type=Template&metadata_fields=UID&metadata_fields=template_description&metadata_fields=template_thumbnail`,
+        },
+    };
 }
 
 export function toggleShowTemplatesModal() {
-  return {
-    type: TOGGLE_SHOW_TEMPLATES_MODAL
-  }
+    return {
+        type: TOGGLE_SHOW_TEMPLATES_MODAL
+    }
+}
+
+export function setThumbnailCallback(callback) {
+    return {
+        type: SET_TEMPLATE_THUMBNAIL_CALLBACK,
+        payload: callback,
+    }
+}
+
+export function createThumbnail(url) {
+    return {
+        type: TRIGGER_TEMPLATE_THUMBNAIL,
+        payload: url,
+    }
 }
