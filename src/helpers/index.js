cecc5be3b174146561053fadb5acb5ce1480da3e
Mike Kühnapfel <16109180+veyxos@users.noreply.github.com>
2025-05-16 15:38:15 +0200

    refactor src/helpers/index.js
    ref #5001

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,36 +1,60 @@
+/**
+ * @param {object} arg0
+ * @param {(cb: ((url: string) => void)) => void} arg0.setThumbnailCallback
+ * @param {() => string} arg0.getPathname
+ * @param {(element: HTMLElement) => Promise<string>} arg0.takeScreenshot
+ * @param {(url: string, content: { template_thumbnail?: { data: string, encoding: string, 'content-type': string, filename: string}}) => void} arg0.updateContent
+ * @param {import('react').RefObject<HTMLElement>} arg0.thumbnailRef
+ * @returns {void}
+ */
 export function initThumbnailHandler({
-     setThumbnailCallback,
-     getPathname,
-     takeScreenshot,
-     updateContent,
-     thumbnailRef,
- }) {
-    setThumbnailCallback((url) => {
-        const maxRetries = 5;
-        let attempts = 0;
+    setThumbnailCallback,
+    getPathname,
+    takeScreenshot,
+    updateContent,
+    thumbnailRef
+}) {
+    const maxRetries = 5;
 
-        const interval = setInterval(() => {
-            attempts++;
+    setThumbnailCallback(
+        /**
+         * @param {string} url
+         * @returns {void}
+         */
+        (url) => {
+            let attempts = 0;
 
-            const pathname = getPathname();
-            if (!pathname.includes('/edit') && pathname.includes(url)) {
-                takeScreenshot(thumbnailRef.current).then((image) => {
-                    const fields = image.match(/^data:(.*);(.*),(.*)$/);
-                    updateContent(url, {
-                        template_thumbnail: {
-                            data: fields[3],
-                            encoding: fields[2],
-                            'content-type': fields[1],
-                            filename: 'thumbnail',
-                        },
-                    });
-                });
+            const interval = setInterval(() => {
+                const pathname = getPathname();
 
-                clearInterval(interval);
-            } else if (attempts >= maxRetries) {
-                console.warn('Max retries reached. Stopping interval.');
-                clearInterval(interval);
-            }
-        }, 500);
-    });
+                if (++attempts === maxRetries) {
+                    console.warn('Max retries reached. Stopping interval.');
+                    clearInterval(interval);
+                    return;
+                }
+
+                // Used RegEx to differentiate between edit view and random paths containing the substring /edit
+                if (!/\/edit(?:$|#|\?|\/)/.test(url) && pathname.includes(url)) {
+                    takeScreenshot(thumbnailRef.current)
+                        .then((image) => {
+                            const { data, encoding, contentType } = image.match(/^data:(?<contentType>.*);(?<encoding>.*),(?<data>.*)$/).groups;
+
+                            updateContent(url, {
+                                template_thumbnail: {
+                                    data,
+                                    encoding,
+                                    'content-type': contentType,
+                                    filename: 'thumbnail',
+                                },
+                            });
+                        })
+                        .catch((err) => {
+                            console.warn('Error whle trying to set thumbnail.', err);
+                        });
+
+                    clearInterval(interval);
+                }
+            }, 500);
+        }
+    );
 }
