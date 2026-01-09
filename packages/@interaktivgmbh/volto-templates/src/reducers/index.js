74eb506e1be56c36d6fd0246ed1a50c2971e89c1
mxurice-rm <reim@interaktiv.de>
2026-01-09 14:48:50 +0100

    Add template functionality with block restrictions

---
+++ packages/@interaktivgmbh/volto-templates/src/reducers/index.js
@@ -0,0 +1,10 @@
+/* istanbul ignore file */
+import templates from './templates';
+import templateContainer from './templateContainer';
+
+const reducers = {
+  templates,
+  templateContainer,
+};
+
+export default reducers;
