8364e79ec8b290f1a7820b53b1591cb7c8056fb1
Julian Cyliax <cyliax@interaktiv.de>
2026-01-20 12:38:17 +0100

    Add i18n support and block settings schema to volto-templates

--- packages/volto-templates/src/messages.js
+++ packages/volto-templates/src/messages.js
@@ -45,6 +45,55 @@ const messages = defineMessages({
     id: 'openImageInNewTab',
     defaultMessage: 'Open image in new tab',
   },
+  blockSettings: {
+    id: 'Block settings',
+    defaultMessage: 'Block settings',
+  },
+  default: {
+    id: 'Default',
+    defaultMessage: 'Default',
+  },
+  placeholder: {
+    id: 'Helper text',
+    defaultMessage: 'Helper text',
+  },
+  placeholderDescription: {
+    id: 'A short hint that describes the expected value within this block',
+    defaultMessage:
+      'A short hint that describes the expected value within this block',
+  },
+  required: {
+    id: 'Required',
+    defaultMessage: 'Required',
+  },
+  requiredDescription: {
+    id: "Don't allow deletion of this block",
+    defaultMessage: "Don't allow deletion of this block",
+  },
+  fixed: {
+    id: 'Fixed position',
+    defaultMessage: 'Fixed position',
+  },
+  fixedDescription: {
+    id: 'Disable drag & drop on this block',
+    defaultMessage: 'Disable drag & drop on this block',
+  },
+  disableNewBlocks: {
+    id: 'Disable new blocks',
+    defaultMessage: 'Disable new blocks',
+  },
+  disableNewBlocksDescription: {
+    id: 'Disable creation of new blocks after this block',
+    defaultMessage: 'Disable creation of new blocks after this block',
+  },
+  readOnly: {
+    id: 'Read-only',
+    defaultMessage: 'Read-only',
+  },
+  readOnlyDescription: {
+    id: 'Disable editing on this block',
+    defaultMessage: 'Disable editing on this block',
+  },
 });
 
 export default messages;
